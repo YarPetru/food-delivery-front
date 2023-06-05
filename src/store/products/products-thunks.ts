074593c879eced9a01a58b6ea82c13e8a6c83898
Yaroslav Petrukhin <yar.petrukhin@gmail.com>
2023-06-05 15:24:37 +0300

    endpoint actualized after backend deploy

--- src/store/products/products-thunks.ts
+++ src/store/products/products-thunks.ts
@@ -1,7 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:9999/api/';
+// axios.defaults.baseURL = 'http://localhost:9999/api/';
+axios.defaults.baseURL = 'https://food-delivery-back-9ix7.onrender.com/api/';
 
 export const fetchProducts = createAsyncThunk('products/fetch', async () => {
   const res = await axios.get('/products');
